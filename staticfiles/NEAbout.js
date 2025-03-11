92dd2824dc311113886735667034369941a8f90b
Mark Nashed <mnashed@simpleciti.com>
2025-03-11 12:54:12 -0400

    none

--- staticfiles/NEAbout.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".accordion__item").forEach(item => {
-    item.addEventListener("click", function() {
-        // Remove active class from all items
-        document.querySelectorAll(".accordion__item").forEach(i => i.classList.remove("active"));
-
-        // Toggle the clicked item
-        this.classList.add("active");
-    });
-});
--- staticfiles/foradvisors.js
+++
@@ -1,8 +0,0 @@
-const script = document.getElementById('init');
-
-    script.onload = () => {
-        putInitScript('runPlugin');
-        runPlugin('ctrader-plugin-root', {"route":"/market-chart/?s=EURUSD&period=H1&charttype=candlestick&palettename=dark&lang=en&w=830&h=700"});
-    };
-
-    
