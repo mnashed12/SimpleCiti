8a64a86818ed5834c9c2ac3031f43dddc2d86f0e
Mark Nashed <mnashed@simpleciti.com>
2025-11-13 14:04:29 -0500

    Routing: disable all Django SE/* routes in HomePage/urls.py so React SPA catch-all handles them; updated backend/urls.py comment

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include # type: ignore
 from django.contrib import sitemaps
 from django.contrib.sitemaps.views import sitemap
 from HomePage.sitemaps import StaticViewSitemap
+from HomePage.react_urls import react_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,13 +13,14 @@ sitemaps_dict = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/se/', include('HomePage.api_urls')),  # SE React API endpoints
+    path('accounts/', include('allauth.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps_dict}, name='django.contrib.sitemaps.views.sitemap'),
+] + react_urlpatterns + [
+    # HomePage.urls should NOT include any SE/* routes - those are handled by React catch-all above
     path('', include('HomePage.urls')),
     path('SC/', include('HomePage.urls')),
     path('SM/', include('HomePage.urls')),
-    path('SE/', include('HomePage.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('', include('HomePage.urls')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps_dict}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 # Serve static and media files during development
