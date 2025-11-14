f9cacdc16b47c823aa4189c75f0e5db6f16c06ce
Mark Nashed <mnashed@simpleciti.com>
2025-11-14 15:22:11 -0500

    Fix: pass DEBUG context to se_react.html template for HMR to activate in dev mode

--- HomePage/react_views.py
+++ HomePage/react_views.py
@@ -3,6 +3,7 @@ Views to serve React SPA for SE section
 """
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
+from django.conf import settings
 
 @never_cache
 def se_react_app(request, path=''):
@@ -10,4 +11,4 @@ def se_react_app(request, path=''):
     Serve the React SPA for all /SE/ routes
     This handles client-side routing
     """
-    return render(request, 'se_react.html')
+    return render(request, 'se_react.html', {'debug': settings.DEBUG})
